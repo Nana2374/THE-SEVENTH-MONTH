912bd64767bde1a49d108d3c8c88d7aca0432d9b
Sze-Ching <oszeching07@gmail.com>
2025-10-25 19:12:15 +0800

    Edit sizes of assets
    - Made assets a bit bigger/ darker, some of the overlays are too light
    - Add bell sfx
    - move overlay message png into folder

--- The Seventh Month/Assets/Scripts/SubmitSprite.cs
+++ The Seventh Month/Assets/Scripts/SubmitSprite.cs
@@ -5,11 +5,15 @@ public class SubmitSprite : MonoBehaviour
     public SolutionChecker solutionChecker; // assign in inspector
     public CameraMovement cameraMovement;   // assign in inspector
 
+    public AudioClip bellSound;
+    public AudioSource audioSource;   // assign in Inspector
+
     void OnMouseDown()
     {
         if (solutionChecker != null)
         {
             solutionChecker.SubmitSolution();
+            audioSource.PlayOneShot(bellSound);
             Debug.Log("Submit sprite clicked!");
         }
 
